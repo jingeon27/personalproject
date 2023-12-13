a60a0682d6e51612fbf3f806c5b7f85375bce4e7
김진건 <87012465+jingeon27@users.noreply.github.com>
2023-12-13 20:07:20 +0900

    be setting

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -4,18 +4,11 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
-    "eslint:recommended",
-    "prettier",
-    "eslint-config-turbo",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
-  plugins: ["only-warn"],
-  globals: {
-    React: true,
-    JSX: true,
-  },
   env: {
     node: true,
     jest: true,
@@ -27,15 +20,10 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
-  overrides: [
-    {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
-  ],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 };
